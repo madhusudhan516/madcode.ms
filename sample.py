df3dd40da1ce4b70de1d6089c3da3d5501f3743f
madhusudhan516 <madhusudhan.bathina516@gmail.com>
2022-05-06 13:13:19 +0530

    added sample.py

---
+++ sample.py
@@ -0,0 +1,18 @@
+# pylint: disable=invalid-name
+"""This is a sample template."""
+
+my_name = "madhu sudhan rao"
+
+
+def my_function():
+    "Disiplays my name."
+    print(my_name)
+
+
+def main():
+    "Calling the my_function()."
+    my_function()
+
+
+if __name__ == "__main__":
+    main()
